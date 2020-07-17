59207614dd08815c561f2aa77b81fc9806cfa15b
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2020-07-17 19:27:42 +0400

    parse university

--- parsing.go
+++ parsing.go
@@ -130,20 +130,23 @@ func parsePage(wg *sync.WaitGroup, pageUrl string) []*University {
 }
 
 func searchUniversities(node *html.Node) []*University {
+	universitiesMainUrl := UniversitiesSite[:20]
+
 	if isDiv(node, "sideContent") {
 		var unis []*University
 		for c := node.FirstChild; c != nil; c = c.NextSibling {
-			if isDiv(c, "col-md-12 itemVuz") {
-				title := strings.TrimSpace(c.FirstChild.FirstChild.FirstChild.LastChild.FirstChild.Data)
-				uni := &University{
-					Name: title,
+			if isDiv(c, "col-md-12 itemVuz") || isDiv(c, "col-md-12 itemVuzPremium") {
+				if a := c.FirstChild.FirstChild.FirstChild; isElem(a, "a") {
+					uniSite := universitiesMainUrl + getAttr(a, "href")
+					uni := parseUniversity(uniSite)
+					unis = append(unis, uni)
 				}
-				unis = append(unis, uni)
 			}
 		}
 
 		return unis
 	}
+
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
 		if unis := searchUniversities(c); unis != nil {
 			return unis
@@ -151,6 +154,115 @@ func searchUniversities(node *html.Node) []*University {
 	}
 
 	return nil
+}
+
+func parseUniversity(uniSite string) *University {
+	log.Println("sending request to " + uniSite)
+	if response, err := http.Get(uniSite); err != nil {
+		log.Println("request to " + uniSite + " failed", "error: ", err)
+	} else {
+		defer response.Body.Close()
+		status := response.StatusCode
+		log.Println("got response from " + uniSite, "status", status)
+		if status == http.StatusOK {
+			if doc, err := html.Parse(response.Body); err != nil {
+				log.Println("invalid HTML from " + uniSite, "error", err)
+			} else {
+				log.Println("HTML from " + uniSite + " parsed successfully")
+				return searchUniversity(doc)
+			}
+		}
+	}
+
+	return nil
+}
+
+func searchUniversity(node *html.Node) *University {
+	if isDiv(node, "content clearfix") {
+		cs := getChildren(node)
+		title, dormitary, militaryDep, description := searchUniInfo(cs[len(cs) - 2])
+		phone, adress, email, site := searchUniInfo2(cs[len(cs) - 1])
+		uni := &University{
+			Name: title,
+			Description: description,
+			Site: site,
+			Email: email,
+			Adress: adress,
+			Phone: phone,
+			MilitaryDep: militaryDep,
+			Dormitary: dormitary,
+		}
+
+		return uni
+	}
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if uni := searchUniversity(c); uni != nil {
+			return uni
+		}
+	}
+
+	return nil
+}
+
+func searchUniInfo(node *html.Node) (string, bool, bool, string) {
+	if isDiv(node, "mainSlider-left") {
+		cs := getChildren(node)
+		title := strings.TrimSpace(cs[0].FirstChild.Data)
+		description := ""
+		descriptionContent := cs[6].FirstChild
+		if isText(descriptionContent) {
+			description = strings.TrimSpace(descriptionContent.Data)
+		}
+		optionsDivCs := getChildren(cs[1])
+		dormitary := optionsDivCs[1].FirstChild.Data == CheckMark
+		militaryDep := optionsDivCs[5].FirstChild.Data == CheckMark
+
+		return title, dormitary, militaryDep, description
+	}
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if title, dorm, milDep, desc := searchUniInfo(c); title != "" {
+			return title, dorm, milDep, desc
+		}
+	}
+
+	return "", false, false, ""
+}
+
+func searchUniInfo2(node *html.Node) (string, string, string, string) {
+	if isDiv(node, "col-lg-6 col-md-6 col-xs-12 col-sm-6") {
+		cs := getChildren(node)
+
+		phone, adress, email, site := "", "", "", ""
+
+		phoneContent := cs[1].LastChild.FirstChild
+		if isText(phoneContent) {
+			phone = phoneContent.Data
+		}
+		adressContent := cs[2].LastChild.FirstChild
+		if isText(adressContent) {
+			adress = adressContent.Data
+		}
+		emailContent := cs[3].LastChild.FirstChild
+		if isText(emailContent) {
+			email = emailContent.Data
+		}
+		siteContent := cs[4].LastChild.FirstChild
+		if isText(siteContent) {
+			site = siteContent.Data
+		}
+
+		return phone, adress, email, site
+	}
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if phone, addr, email, site := searchUniInfo2(c); phone != "" {
+			return phone, addr, email, site
+		}
+	}
+
+	return "", "", "", ""
 }
 
 func main() {
