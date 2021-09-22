fff270632178fefd3185e2cc201930d4391d90a5
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2021-09-22 21:50:37 +0400

    add constants file

---
+++ constants.go
@@ -0,0 +1,21 @@
+package main
+
+const (
+	// telegtam constants
+	BotToken = "BotToken"
+	CreatorID int64 = 12345678
+
+	// database constants
+	Host = "Host"
+	Port = 1234
+	User = "User"
+	Password = "Password"
+	DBname = "DBname"
+
+	// parse constants
+	UniversitiesSite = "https://vuzopedia.ru/vuz/"
+	RatingQsSite = "https://www.topuniversities.com/qs-world-university-rankings"
+	CheckMark = ""
+	BachelorSpecialitiesSite = "https://base.garant.ru/70480868/53f89421bbdaf741eb2d1ecc4ddb4c33/"
+	SpecialistSpecialitiesSite = "https://base.garant.ru/70480868/3e22e51c74db8e0b182fad67b502e640/"
+)
